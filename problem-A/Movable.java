dedb6658b6ceabf82e94cb27b6e6c84d44351ec6
Madlson <120780771+Madlson@users.noreply.github.com>
2023-02-26 00:55:17 +0600

    Give a name and ID
    Name: Madi Meirambay
    ID: 210103449

--- problem-A/Movable.java
+++ problem-A/Movable.java
@@ -1,6 +1,6 @@
 public interface Movable {
-    void moveUp();
-    void moveDown();
-    void moveLeft();
-    void moveRight();
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
 }
