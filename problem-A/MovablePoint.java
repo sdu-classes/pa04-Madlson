cc0914de1986cf05a9a6903261494409888f3871
Madlson <120780771+Madlson@users.noreply.github.com>
2023-02-26 00:52:46 +0600

    Update MovablePoint.java

---
+++ problem-A/MovablePoint.java
@@ -0,0 +1,34 @@
+class MovablePoint implements Movable {
+    int x, y, xSpeed, ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public String toString() {
+        return String.format("(%d,%d),speed=(%d,%d)", x, y, xSpeed, ySpeed);
+    }
+
+    @Override
+    public void moveUp() {
+        y -= ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        y += ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        x -= xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        x += xSpeed;
+    }
+}
